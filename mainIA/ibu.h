762b16c5b11ed30f4fe5c8a328962089e4bc57c8
mrendy58 <derendyyy58@gmail.com>
2023-05-22 20:54:22 +0700

    membuat class ibu yg bersift public

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -7,7 +7,7 @@ public:
 	vector <anak* > daftar_anak;
 
 	ibu(string pNama) :nama(pNama) {
-		cout << "Ibu \"" << nama << "\" ada\n";
+		cout << "Ibu \"" << nama << "\" ada\n"; 
 	}
 	~ibu() {
 		cout << "Ibu \"" << nama << "\" tidak ada\n";
