810e787865273deed3ca8974d3b600f35171cf78
mrendy58 <derendyyy58@gmail.com>
2023-05-22 21:20:09 +0700

    membuat kelas, konduktor jantung

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+
+
+class jantung {
+public:
+	jantung() {
+		cout << "Jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "Jantung dimatikan\n";
+	}
+};
+#endif
